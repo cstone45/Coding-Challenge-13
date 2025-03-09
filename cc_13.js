28bbcf00809a6eab35ea7008d40a84afadad02b1
cstone45 <cstone45@usf.edu>
2025-03-09 03:20:06 -0400

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,2 +1,11 @@
 //Task 1: Base Structure Control
 // See Index.Html File 
+
+//Task 2: Adding Employee Cards Dynamically
+const employeeContainerById = document.getElementById("employeeContainer")
+const employeeCard = create.Element("div");
+employeeCard.setAttribute("id", "employeeCard");
+employeeCard.setAttribute("class", "metric-card");
+employeeCard.innerHTML = "<h2>Employee Name:</h2><p>Employees Position:</p>";
+<button onclick="removeEmployeeItem('Employee')">Remove Employee</button>;
+employeeContainer.appendChild(employeeCard);
