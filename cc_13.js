91a57567aaa7df541570bc760468b9fbb523d15c
cstone45 <cstone45@usf.edu>
2025-03-09 05:40:59 -0400

    Task 3 - Bulk Update on Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -9,3 +9,11 @@ employeeCard.setAttribute("class", "metric-card");
 employeeCard.innerHTML = "<h2>Employee Name:</h2><p>Employees Position:</p>";
 <button onclick="removeEmployeeItem('Employee')">Remove Employee</button>;
 employeeContainer.appendChild(employeeCard);
+
+//Task 3: Converting NodeLists to Arrays for Bulk Updates
+const employeeCard = document.querySelector("#employeeCard");
+const arrayOfEmployeeCard = Array.from(employeeCardNodeList);
+employeeCardArray.forEach(card => {
+    background-color: dark Blue;
+    border-radius: 6px;
+})
